ebe7f8c27113eecde2c38f26feffcaeb6e7c27de
kanaarima <137962405+kanaarima@users.noreply.github.com>
2024-02-25 11:19:51 +0100

    Fix session leak

--- app.py
+++ app.py
@@ -10,14 +10,14 @@ app = FastAPI()
 
 @app.get("/user")
 async def user(server: str, id: int):
-    with database.session as session:
+    with database.managed_session() as session:
         if (user := session.get(DBUser, (id, server))):
             return user
         raise HTTPException(status_code=404, detail="Item not found")
 
 @app.get("/beatmap/{id}")
 async def beatmap(id: int):
-    with database.session as session:
+    with database.managed_session() as session:
         if (beatmap := session.get(DBBeatmap, id)):
             session.expunge(beatmap.beatmapset)
             return beatmap
@@ -25,7 +25,7 @@ async def beatmap(id: int):
 
 @app.get("/beatmapset/{id}")
 async def beatmapset(id: int):
-    with database.session as session:
+    with database.managed_session() as session:
         if (beatmapset := session.get(DBBeatmapset, id)):
             for beatmap in beatmapset.beatmaps:
                 session.expunge(beatmap)
@@ -34,14 +34,14 @@ async def beatmapset(id: int):
 
 @app.get("/beatmap/pack/{tag}")
 async def beatmap_pack(tag: str):
-    with database.session as session:
+    with database.managed_session() as session:
         if (pack := session.get(DBBeatmapPack, tag)):
             return pack
         raise HTTPException(status_code=404, detail="Item not found")
 
 @app.get("/beatmap/pack/{tag}/completion")
 async def user_beatmap_pack_completion(tag: str, server: str, user_id: int, mode: int, relax: int):
-    with database.session as session:
+    with database.managed_session() as session:
         sets = session.query(DBBeatmapset).filter(DBBeatmapset.pack_tags.any(tag)).all()
         result = {
             'completed': list(),
@@ -57,7 +57,7 @@ async def user_beatmap_pack_completion(tag: str, server: str, user_id: int, mode
 
 @app.get("/user/stats")
 async def user_stats(server: str, id: int, mode: int, relax: int, date: date = date.today()):
-    with database.session as session:
+    with database.managed_session() as session:
         if (stats := session.query(DBStats).filter(
             DBStats.user_id == id,
             DBStats.server == server,
@@ -70,7 +70,7 @@ async def user_stats(server: str, id: int, mode: int, relax: int, date: date = d
 
 @app.get("/user/stats/all")
 async def user_stats_all(server: str, id: int, mode: int, relax: int, date: date = date.today()):
-    with database.session as session:
+    with database.managed_session() as session:
         if (stats := session.query(DBStats.date).filter(
             DBStats.user_id == id,
             DBStats.server == server,
@@ -82,7 +82,7 @@ async def user_stats_all(server: str, id: int, mode: int, relax: int, date: date
 
 @app.get("/score")
 async def score(server: str, id: int):
-    with database.session as session:
+    with database.managed_session() as session:
         if (score := session.get(DBScore, (id, server))):
             return score
         raise HTTPException(status_code=404, detail="Item not found")
@@ -90,7 +90,7 @@ async def score(server: str, id: int):
 @app.get("/score/search")
 async def query_scores(query: str, page: int = 1, length: int = 100):
     length = min(1000, length)
-    with database.session as session:
+    with database.managed_session() as session:
         q = build_query(session.query(DBScore), DBScore, query.split(","))
         if (scores := q.offset((page - 1) * length).limit(length).all()):
             return [item for item in scores]
@@ -99,7 +99,7 @@ async def query_scores(query: str, page: int = 1, length: int = 100):
 @app.get("/leaderboard/{type}")
 async def leaderboard(type: str, server: str, mode: int, relax: int, query: str = "", page: int = 1, length: int = 100):
     length = min(1000, length)
-    with database.session as session:
+    with database.managed_session() as session:
         q = session.query(DBStatsCompact).filter(
             DBStatsCompact.server == server,
             DBStatsCompact.mode == mode,
